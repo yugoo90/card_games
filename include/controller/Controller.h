c3fab7e6db48a84affcb7b2c4969fb7f54c090f2
yugoo90 <uosuji54@gmail.com>
2025-06-30 09:44:27 -0600

    deleted unnecessary files and folders

---
+++ include/controller/Controller.h
@@ -0,0 +1,18 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include "oatpp/web/server/api/ApiController.hpp"
+#include "oatpp/core/macro/codegen.hpp"
+
+#include <memory>
+#include <iostream>
+#include <vector>
+#include "UI.h"
+
+#include OATPP_CODEGEN_BEGIN(ApiController)
+
+
+
+#include #include OATPP_CODEGEN_END(ApiController)
+
+#endif
